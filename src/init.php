0dcbfc520e8c73e2badcb53ac73dacb1adef2ab5
Blabla404 <blabla404@free.fr>
2014-01-16 00:19:42 +0100

    switch from sqlite syntaxe to mysql

--- src/init.php
+++ src/init.php
@@ -10,17 +10,19 @@ catch (Exception $e)
 }
 
 $bdd->exec('DROP TABLE user');
-$bdd->exec('CREATE TABLE user(id INTEGER AUTO_INCREMENT PRIMARY KEY,pseudo VARCHAR, password VARCHAR, email VARCHAR, group INT, idUva VARCHAR)');
+$bdd->exec('CREATE TABLE user(id INT AUTO_INCREMENT PRIMARY KEY,pseudo VARCHAR(255), password VARCHAR(255), email VARCHAR(255), admin BOOL, idUva VARCHAR(255))');
 $bdd->exec('INSERT INTO user VALUES ("Bibi", "coucou", "paf", 1, "43939")');
 
 $bdd->exec('DROP TABLE problem');
-$bdd->exec('CREATE TABLE problem(id INTEGER AUTO_INCREMENT PRIMARY KEY,title VARCHAR, url VARCHAR, site INT)');
+$bdd->exec('CREATE TABLE problem(id INT AUTO_INCREMENT PRIMARY KEY,title VARCHAR(255), url VARCHAR(255), site INT)');
 
 $bdd->exec('DROP TABLE td');
-$bdd->exec('CREATE TABLE td(id INTEGER AUTO_INCREMENT PRIMARY KEY,title VARCHAR, dueDate DATE)');
+$bdd->exec('CREATE TABLE td(id INT AUTO_INCREMENT PRIMARY KEY,title VARCHAR(255), dueDate DATE)');
 
 $bdd->exec('DROP TABLE assignement');
-$bdd->exec('CREATE TABLE assignement(id INTEGER AUTO_INCREMENT PRIMARY KEY, idProblem INT, idTD INT');
+$bdd->exec('CREATE TABLE assignement(id INT AUTO_INCREMENT PRIMARY KEY, idProblem INT, idTD INT)');
+
+
 
 header('Location: index.php'); 
 ?>
