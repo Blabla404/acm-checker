f287ddab9441a4552354ce81a4fd3ed0f986616e
Blabla404 <blabla404@free.fr>
2014-01-16 23:18:11 +0100

    automate mysql in vagrant startup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y nginx sqlite3 php5-fpm php5-sqlite php-apc
+apt-get install -y nginx php5-fpm php-apc php5-mysql
 service nginx start
 rm -f /etc/nginx/sites-available/default
 ln -fs /vagrant/conf/default /etc/nginx/sites-available/default
 rm -f /etc/php5/fpm/pool.d/www.conf
 ln -fs /vagrant/conf/www.conf /etc/php5/fpm/pool.d/www.conf
-sudo service php5-fpm reload && sudo service nginx reload
-
+echo "mysql-server-5.5 mysql-server/root_password_again password root" | debconf-set-selections
+echo "mysql-server-5.5 mysql-server/root_password password root" | debconf-set-selections
+apt-get -y install mysql-server
+mysql --user=root --password=root mysql < /vagrant/start.sql
+service php5-fpm restart && service nginx restart
