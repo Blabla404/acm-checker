644b58639dcc1fad59c6f17db0c24a9a7760d1d1
Blabla404 <blabla404@free.fr>
2014-01-19 00:00:40 +0100

    ajout manuel du premier td

--- src/debug.php
+++ src/debug.php
@@ -5,6 +5,15 @@ include_once('connectdb.php');
 $res = $bdd->query('SELECT * FROM user');
 while($data = $res->fetch()){
   print_r($data);
+  echo('<br/>');
+}
+
+echo('<br/><br/>');
+
+$res = $bdd->query('SELECT * FROM problem');
+while($data = $res->fetch()){
+  print_r($data);
+  echo('<br/>');
 }
 
 include('footer.php');
--- src/init.php
+++ src/init.php
@@ -4,17 +4,23 @@ include_once('connectdb.php');
 require('lib/password.php');
 
 $bdd->exec('DROP TABLE user');
-$bdd->exec('CREATE TABLE user(id INT AUTO_INCREMENT PRIMARY KEY,pseudo VARCHAR(255), password VARCHAR(255), email VARCHAR(255), admin INT, idUva VARCHAR(255))');
+$bdd->exec('CREATE TABLE user(id INT AUTO_INCREMENT PRIMARY KEY,pseudo VARCHAR(255), password VARCHAR(255), email VARCHAR(255), admin BOOL, idUva VARCHAR(255))');
 $bdd->exec('INSERT INTO user VALUES ("", "admin", "'. password_hash('admin', PASSWORD_DEFAULT). '", "admin@admin.com", 1, 42)');
 
 $bdd->exec('DROP TABLE problem');
-$bdd->exec('CREATE TABLE problem(id INT AUTO_INCREMENT PRIMARY KEY,title VARCHAR(255), url VARCHAR(255), site INT)');
+$bdd->exec('CREATE TABLE problem(id INT AUTO_INCREMENT PRIMARY KEY,title VARCHAR(255), url VARCHAR(255), site INT, bonus BOOL, idTD INT)');
+
+$bdd->exec('INSERT INTO problem VALUES ("", "10055", "http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=996", 1, 0, 1)');
+$bdd->exec('INSERT INTO problem VALUES ("", "272", "http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=208", 1, 0, 1)');
+$bdd->exec('INSERT INTO problem VALUES ("", "11559", "http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=2595", 1, 0, 1)');
+$bdd->exec('INSERT INTO problem VALUES ("", "10420", "http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=1361", 1, 0, 1)');
+$bdd->exec('INSERT INTO problem VALUES ("", "12205", "http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=276&page=show_problem&problem=3357", 1, 0, 1)');
+$bdd->exec('INSERT INTO problem VALUES ("", "12394", "http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=280&page=show_problem&problem=3816", 1, 1, 1)');
+$bdd->exec('INSERT INTO problem VALUES ("", "12550", "http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=443&page=show_problem&problem=3995", 1, 1, 1)');
 
 $bdd->exec('DROP TABLE td');
 $bdd->exec('CREATE TABLE td(id INT AUTO_INCREMENT PRIMARY KEY,title VARCHAR(255), dueDate DATE)');
 
-$bdd->exec('DROP TABLE assignement');
-$bdd->exec('CREATE TABLE assignement(id INT AUTO_INCREMENT PRIMARY KEY, idProblem INT, idTD INT)');
 
 header('Location: index.php');
 ?>
