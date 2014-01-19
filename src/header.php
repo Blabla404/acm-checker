e068fca64f7adfad815a87e0145b2b944cf8d516
Blabla404 <blabla404@free.fr>
2014-01-19 01:22:04 +0100

    correction de typos

--- src/header.php
+++ src/header.php
@@ -44,8 +44,8 @@
 	<li class="dropdown">
 	  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Liens <b class="caret"></b></a>
 	  <ul class="dropdown-menu">
-            <li><a href="https://docs.google.com/spreadsheet/ccc?key=0ApMTqWB_E_MxdHZmUWZOeTVIeVoySGFOYUFxTHVJNVE&usp=sharing">Google Doc</a></li>
-            <li><a href="https://drive.google.com/folderview?id=0B5MTqWB_E_MxZktYZmJjTFl0LWs&usp=sharing">Testcases</a></li>
+            <li><a href="https://docs.google.com/spreadsheet/ccc?key=0ApMTqWB_E_MxdHZmUWZOeTVIeVoySGFOYUFxTHVJNVE&amp;usp=sharing">Google Doc</a></li>
+            <li><a href="https://drive.google.com/folderview?id=0B5MTqWB_E_MxZktYZmJjTFl0LWs&amp;usp=sharing">Testcases</a></li>
             <li class="divider"></li>
             <li><a href="lien.php">Sites utiles</a></li>
 	  </ul>
